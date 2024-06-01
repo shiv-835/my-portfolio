b877bc942593f239c11a9705320a626943e9e57f
shiv-835 <shiv.chauhan1518@gmail.com>
2024-06-02 00:09:33 +0530

    Initial Commit

---
+++ js/smoothscroll.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('a[href^="#"]');
+    
+    for (const link of links) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+            
+            if (targetElement) {
+                const offsetTop = targetElement.offsetTop;
+                
+                window.scrollTo({
+                    top: offsetTop,
+                    behavior: 'smooth' // Smooth scrolling behavior
+                });
+            }
+        });
+    }
+});
